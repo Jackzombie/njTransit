1fedae2cc5839c368156cd428fc96a7aaa3826f0
Jackson Flynn <jacksonaflynn@gmail.com>
2025-09-29 15:01:17 -0400

    replacing server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
+// SPA fallback route
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
